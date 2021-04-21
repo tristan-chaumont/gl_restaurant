c2a4748f76ca7ef378f95a4b3b8b0cf5030bdd25
Tristan Chaumont <tristan.chaumont68@gmail.com>
2021-04-21 21:22:22 +0200

    Ajout du model Tables et création du repository vide

---
+++ src/main/java/fr/ul/miage/gl_restaurant/model/Tables.java
@@ -0,0 +1,35 @@
+package fr.ul.miage.gl_restaurant.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Tables")
+@Data
+public class Tables {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long tableId;
+
+    @Column(name = "floor")
+    private long floor;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "places")
+    private long places;
+
+    @ManyToOne
+    private Users users;
+
+    public Tables() {}
+
+    public Tables(long floor, String state, long places) {
+        this.floor = floor;
+        this.state = state;
+        this.places = places;
+    }
+}
