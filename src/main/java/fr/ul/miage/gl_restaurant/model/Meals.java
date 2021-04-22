52d8da96616132f19c4793b93be8e08b097bed47
Tristan Chaumont <tristan.chaumont68@gmail.com>
2021-04-22 13:55:51 +0200

    ft-005 - Ajout du model Meals et du Repository

---
+++ src/main/java/fr/ul/miage/gl_restaurant/model/Meals.java
@@ -0,0 +1,36 @@
+package fr.ul.miage.gl_restaurant.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "Meals")
+@Data
+public class Meals {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long mealId;
+
+    @Column(name = "customersNb")
+    private long customersNb;
+
+    @Column(name = "startDate")
+    private Timestamp startDate;
+
+    @Column(name = "mealDuration")
+    private long mealDuration;
+
+    @ManyToOne
+    private Tables table;
+
+    public Meals() {}
+
+    public Meals(long customersNb, Timestamp startDate, long mealDuration) {
+        this.customersNb = customersNb;
+        this.startDate = startDate;
+        this.mealDuration = mealDuration;
+    }
+}
---
+++ src/main/java/fr/ul/miage/gl_restaurant/model/MealsRepository.java
@@ -0,0 +1,6 @@
+package fr.ul.miage.gl_restaurant.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MealsRepository extends JpaRepository<Meals, Long> {
+}
