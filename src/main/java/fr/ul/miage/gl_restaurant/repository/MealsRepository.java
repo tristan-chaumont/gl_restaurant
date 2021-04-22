6ba54bce56396825bdfafe99c39a101c415f2294
Tristan Chaumont <tristan.chaumont68@gmail.com>
2021-04-22 14:08:59 +0200

    ft-005 - Déplacement de la classe MealsRepository dans le package repository

--- src/main/java/fr/ul/miage/gl_restaurant/repository/MealsRepository.java
+++ src/main/java/fr/ul/miage/gl_restaurant/repository/MealsRepository.java
@@ -1,5 +1,6 @@
-package fr.ul.miage.gl_restaurant.model;
+package fr.ul.miage.gl_restaurant.repository;
 
+import fr.ul.miage.gl_restaurant.model.Meals;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MealsRepository extends JpaRepository<Meals, Long> {
