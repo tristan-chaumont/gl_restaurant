a8afbd3e96f54c28dce30472a3989dae2d1c68b7
Tristan Chaumont <tristan.chaumont68@gmail.com>
2021-04-24 20:58:25 +0200

    ft-003 <test> - Tests de connexion à la DB
    Un test vérifie si deux connexions sont les mêmes, l'autre vérifie que la connexion est bien null si elle n'est pas lancée.
    
    Ferme #US003

---
+++ src/test/java/fr/ul/miage/gl_restaurant/jdbc/TestDbAccess.java
@@ -0,0 +1,37 @@
+package fr.ul.miage.gl_restaurant.jdbc;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class TestDbAccess {
+
+    Connection connection1, connection2;
+
+    @Test
+    @DisplayName("Deux connexions lancées")
+    void verifyBothConnectionsAreTheSame() {
+        connection1 = DbAccess.getInstance();
+        connection2 = DbAccess.getInstance();
+        assertThat(connection1, is(connection2));
+    }
+
+    @Test
+    @DisplayName("Aucune connexion")
+    void verifyConnectionIsNull() {
+        assertNull(connection1);
+        assertNull(connection2);
+    }
+
+    @AfterEach
+    void tearDown() {
+        connection1 = null;
+        connection2 = null;
+    }
+}
