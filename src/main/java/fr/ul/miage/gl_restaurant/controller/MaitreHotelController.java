dbab82bd3ad65c873a617bd4966bec79517ad884
Tristan Chaumont <tristan.chaumont68@gmail.com>
2021-05-11 18:31:23 +0200

    ft-021 <controller> - Ajout des premières actions dans MaitreHotelController
    - Se déconnecter

--- src/main/java/fr/ul/miage/gl_restaurant/controller/MaitreHotelController.java
+++ src/main/java/fr/ul/miage/gl_restaurant/controller/MaitreHotelController.java
@@ -1,23 +1,34 @@
 package fr.ul.miage.gl_restaurant.controller;
 
-import fr.ul.miage.gl_restaurant.constants.Environment;
+import fr.ul.miage.gl_restaurant.auth.Authentification;
 import fr.ul.miage.gl_restaurant.constants.TableStates;
 import fr.ul.miage.gl_restaurant.model.Meal;
 import fr.ul.miage.gl_restaurant.model.Table;
 import fr.ul.miage.gl_restaurant.repository.MealRepositoryImpl;
 import fr.ul.miage.gl_restaurant.repository.TableRepositoryImpl;
+import org.apache.commons.text.TextStringBuilder;
 
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.Arrays;
 
-public class MaitreHotelController {
+public class MaitreHotelController extends UserController {
 
     private final TableRepositoryImpl tableRepository;
     private final MealRepositoryImpl mealRepository;
 
-    public MaitreHotelController(){
+    /**
+     * ACTIONS DE L'UTILISATEUR
+     */
+    private static final String ACTION_1 = "1 : Prendre une réservation";
+    private static final String ACTION_2 = "2 : Affecter un client à une table";
+    private static final String ACTION_3 = "3 : Affecter un serveur à une table";
+
+    public MaitreHotelController(Authentification auth) {
+        super(auth);
         tableRepository = TableRepositoryImpl.getInstance();
         mealRepository = MealRepositoryImpl.getInstance();
+        this.actions.addAll(Arrays.asList(ACTION_1, ACTION_2, ACTION_3));
     }
 
     public Meal seatClient(Table table, int nbCustomers){
@@ -28,5 +39,26 @@ public class MaitreHotelController {
             tableRepository.update(table);
         }
         return meal;
+    }
+
+    @Override
+    public String displayActions() {
+        var stringBuilder = new TextStringBuilder();
+        stringBuilder.appendln(super.displayActions());
+        for (String action : actions) {
+            stringBuilder.appendln(action);
+        }
+        return stringBuilder.toString();
+    }
+
+    @Override
+    public void callAction(int action) {
+        switch (action) {
+            case 0:
+                auth.disconnect();
+                break;
+            default:
+                break;
+        }
     }
 }
